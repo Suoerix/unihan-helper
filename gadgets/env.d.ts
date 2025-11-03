3338cfb8dfb8f6276434bf3fcac44ec172aa89ab
PexEric <suoerix@gmail.com>
2025-11-03 14:07:25 +0800

    chore(i18n): specify zh-hk & zh-tw

--- gadgets/env.d.ts
+++ gadgets/env.d.ts
@@ -2,8 +2,30 @@
 /// <reference types="vite/client" />
 
 declare module 'ext.gadget.HanAssist' {
-    export function conv(text: string): string;
-    export function batchConv(obj: Record<string, { hans: string; hant: string }>): Record<string, string>;
+  export type CandidateKey = 'other' | 'zh' | 'hans' | 'hant' | 'cn' | 'tw' | 'hk' | 'sg' | 'mo' | 'my';
+  export type Candidates = Partial<Record<CandidateKey, string>>;
+  /**
+   * Select between candidates based on user language.
+   * @param candidates an object of candidates
+   * @param locale locale, defaults to `wgUserLanguage`
+   * @returns selected value
+   */
+  export function conv(candidates: Candidates, locale?: string): string;
+  /**
+   * Select between candidates based on user variant.
+   * @param candidates an object of candidates
+   * @returns selected value
+   */
+  export function convByVar(candidates: Candidates): string;
+  /**
+   * Perform selection for each item in a candidates dictionary.
+   * @param candidatesDict the dictionary of candidates
+   * @param locale locale, defaults to `wgUserLanguage`
+   * @returns converted candidates dictionary
+   */
+  export function batchConv<T extends string>(
+    candidatesDict: Record<T, string | Candidates>, locale?: string,
+  ): Record<T, string>;
 }
 
 declare module '*.vue' {
